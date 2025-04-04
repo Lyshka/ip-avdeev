30aec6653f011f6546a18f3b2e3b94cfc8498341
Alexei Novitskiy <a.navitski@apeclix.com>
2025-04-05 00:39:39 +0300

    del not uses props: id

--- src/components/Header/Menu/Item.tsx
+++ src/components/Header/Menu/Item.tsx
@@ -4,7 +4,7 @@ interface IItemProps {
     item: IMenu
 }
 
-export const MenuItem: React.FC<IItemProps> = ({ item: { id, link, name } }) => {
+export const MenuItem: React.FC<IItemProps> = ({ item: { link, name } }) => {
     return (
         <li>
             <a href={link} className="text-lg text-white font-bold hover:opacity-80">
