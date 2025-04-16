5c68a053c0ddf85aadc5085ef9df297e109f6dca
Alexei Novitskiy <a.navitski@apeclix.com>
2025-04-16 12:45:42 +0300

    test commit

--- src/components/Delivery/DeliveryItem.tsx
+++ src/components/Delivery/DeliveryItem.tsx
@@ -4,7 +4,7 @@ import { IDelivery } from "interfaces"
 export const DeliveryItem: React.FC<IDelivery> = ({ text }) => {
   return (
     <li className="flex items-center gap-2.5 font-bold text-lg uppercase">
-      <Check2Icon className="shrink-0"/>
+      <Check2Icon className="shrink-0 1"/>
 
       {text}
     </li>
