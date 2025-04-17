0cc8974d24052e6bfeaba95567f2a1deedadf35c
Alexei Novitskiy <a.navitski@apeclix.com>
2025-04-17 18:17:28 +0300

    copy css swiper in css file

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
-import 'swiper/css';
-import 'swiper/css/navigation';
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
